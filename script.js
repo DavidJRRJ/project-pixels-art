6fcc1620ac1313d6d9925b887c082509bd09e0f7
David Junior <david.lourenco94@gmail.com>
2023-01-08 17:56:03 -0300

    update: publicando no meu repositorio

--- script.js
+++ script.js
@@ -4,31 +4,18 @@ let green = document.querySelector('#green');
 let blue = document.querySelector('#blue');
 let clear = document.querySelector('#clear-board');
 let vqv = document.querySelector('#generate-board');
-let size = document.querySelector('#board-size');
+let size;
 
-let box = document.querySelectorAll('.pixel');
-
-for (let i = 0; i < box.length; i++){
-  box[i].addEventListener('click', function(){
-    if (sessionStorage.getItem('cor') === 'red'){
-      box[i].style.backgroundColor = 'red';
-    } 
-    else if (sessionStorage.getItem('cor') === 'black'){
-      box[i].style.backgroundColor = 'black';
-    } 
-    else if (sessionStorage.getItem('cor') === 'green'){
-      box[i].style.backgroundColor = 'green';
-    } 
-    else if (sessionStorage.getItem('cor') === 'blue'){
-      box[i].style.backgroundColor = 'blue';
-    } 
-})
-}
 
 window.onload = function() {
   sessionStorage.setItem('cor', 'black');
   black.classList.add('selected');
+  size = parseInt(document.getElementById('board-size').value);
 }
+
+vqv.addEventListener('click', function(){
+  size = parseInt(document.getElementById('board-size').value);
+})
 
 red.addEventListener('click', function(){
   sessionStorage.setItem('cor', 'red');
@@ -62,12 +49,45 @@ blue.addEventListener('click', function(){
   red.classList.remove('selected');
 })
 
-clear.addEventListener('click', function(){
-  for (let j = 0; j < box.length; j++){
-    box[j].style.backgroundColor = 'white';
-  }
-  // sessionStorage.setItem('cor', 'white');
-})
 
+size =  parseInt(document.getElementById('board-size').value);
+
+  let board = document.getElementById('pixel-board');
+  for (let index = 0; index < 5; index += 1) {
+    const coluna = document.createElement('div');
+    coluna.className = 'coluna';
+    for (let i = 1; i <= 5; i += 1) {
+      const linha = document.createElement('div');
+      board.appendChild(linha);
+      linha.className = 'pixel';
+      coluna.appendChild(linha);  
+    }
+    board.appendChild(coluna); 
+  }
+
+  let box = document.querySelectorAll('.pixel');
+  for (let i = 0; i < box.length; i++){
+    box[i].addEventListener('click', function(){
+      if (sessionStorage.getItem('cor') === 'red'){
+        box[i].style.backgroundColor = 'red';
+      } 
+      else if (sessionStorage.getItem('cor') === 'black'){
+        box[i].style.backgroundColor = 'black';
+      } 
+      else if (sessionStorage.getItem('cor') === 'green'){
+        box[i].style.backgroundColor = 'green';
+      } 
+      else if (sessionStorage.getItem('cor') === 'blue'){
+        box[i].style.backgroundColor = 'blue';
+      } 
+  })
+  }
+
+  clear.addEventListener('click', function(){
+    for (let j = 0; j < box.length; j++){
+      box[j].style.backgroundColor = 'white';
+    }
+    // sessionStorage.setItem('cor', 'white');
+  })
 
 
